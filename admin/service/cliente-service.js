b7d5b2d0f653f4019d44df464539f73fc8d3ebea
Lucas Corte <corte1994.lc@gmail.com>
2021-08-03 13:32:00 -0300

    criação da função para crear uma nova linha

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,3 +1,21 @@
+const createNewLine = () => {
+  const lineNewClient = document.createElement('tr')
+  const content = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td>
+  `;
+
+  lineNewClient.innerHTML = content;
+
+  return lineNewClient;
+}
+
 const http =  new XMLHttpRequest();
 
 http.open('GET', 'http://localhost:3000/profile');

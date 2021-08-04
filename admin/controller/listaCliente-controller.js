6f4355de58a342240c71f304cf9e581e77ebedf1
Lucas Corte <corte1994.lc@gmail.com>
2021-08-04 10:17:26 -0300

    alterações

---
+++ admin/controller/listaCliente-controller.js
@@ -0,0 +1,28 @@
+import { clientService } from "../service/cliente-service";
+
+const createNewLine = (nome, email) => {
+  const lineNewClient = document.createElement('tr')
+  const content = `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td>
+  `;
+
+  lineNewClient.innerHTML = content;
+
+  return lineNewClient;
+}
+
+const table = document.querySelector('[data-tabela]');
+
+clientService.listClients()
+  .then(data => {
+      data.forEach( cliente => {
+        table.appendChild(createNewLine(cliente.nome, cliente.email));
+      });
+  });
--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,24 +1,4 @@
-const createNewLine = (nome, email) => {
-  const lineNewClient = document.createElement('tr')
-  const content = `
-    <td class="td" data-td>${nome}</td>
-    <td>${email}</td>
-    <td>
-        <ul class="tabela__botoes-controle">
-            <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-        </ul>
-    </td>
-  `;
-
-  lineNewClient.innerHTML = content;
-
-  return lineNewClient;
-}
-
-const table = document.querySelector('[data-tabela]');
-
-const listClients = () => {
+const listClients = () => { // pega dados e da uma resposta
 
   return fetch('http://localhost:3000/profile')
   .then (res => {
@@ -27,9 +7,6 @@ const listClients = () => {
 
 }
 
-listClients()
-  .then(data => {
-      data.forEach( cliente => {
-        table.appendChild(createNewLine(cliente.nome, cliente.email));
-      });
-  });
+export const clientService = {
+  listClients
+}
